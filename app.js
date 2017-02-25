d210e0b691e3116836f0e414ef8cbcb5eec065a6
Prashant Koshta <prashantkoshta@gmail.com>
2017-02-26 02:32:26 +0530

    update url.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
     /*res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();*/
-    res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+    res.header('Access-Control-Allow-Origin', "https://pubmatic-demo.herokuapp.com");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     res.header("Access-Control-Allow-Credentials", "true");
